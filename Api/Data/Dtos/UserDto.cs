1f51a313f384001c179155fd93b15520e937bc21
madstone290 <madstone290@gmail.com>
2023-06-02 11:51:56 +0900

    feat IdBeforeCommit 추가

---
+++ Api/Data/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Data.Dtos
+{
+    public record UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public int Point { get; set; }
+        public string Type { get; set; } = string.Empty;
+    }
+}
---
+++ Api/Features/IdBeforeCommit/IdBeforeCommitController.cs
@@ -0,0 +1,64 @@
+﻿using Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Text;
+using System.Text.Json;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using Api.Expressions;
+using Api.Data.Entities;
+using Api.Data.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Features.DynamicSelect
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdBeforeCommitController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IdBeforeCommitController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("AddUser")]
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] UserDto userDto)
+        {
+            User user = new User(userDto.Name);
+            using (var tran = await _context.Database.BeginTransactionAsync())
+            {
+                var result = await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                int id = user.Id;
+
+                if (id % 2 == 0)
+                    await tran.CommitAsync();
+                else
+                    await tran.RollbackAsync();
+            }
+
+            return Ok();
+        }
+
+        [Route("GetUsers")]
+        [HttpPost]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.OrderByDescending(x => x.Id).Select(x => new UserDto() 
+            { 
+                Id  =  x.Id,
+                Name= x.Name,
+                Created = x.Created
+            }).ToListAsync();
+
+            return Ok(users);
+        }
+
+
+
+
+    }
+}
